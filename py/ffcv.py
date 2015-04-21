9d7183dec8674c723fa65b17f3325907c642ae92
joaquincasanova <joaquin.casanova@gmail.com>
2015-04-21 13:35:54 -0400

    playing with python

--- py/ffcv.py
+++ py/ffcv.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import cv2
 
 for imnum in range(1,6):
@@ -16,6 +18,33 @@ for imnum in range(1,6):
     cv2.destroyAllWindows()
 
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    cv2.imshow('image',hsv)
+    h,s,v = cv2.split(hsv)
+    cv2.imshow('image',h)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    cv2.imshow('image',v)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    hist = cv2.calcHist( [hsv], [0, 2], None, [180, 256], [0, 180, 0, 256] )
+    cv2.imshow('hist',hist)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    H = np.linspace(0, 180, 180)
+    V = np.linspace(0, 256, 256)
+    VV,HH = np.meshgrid(V,H)
+    print hist.shape
+    ax.plot_surface(HH,VV,hist)
+    plt.show()
+    cv2.waitKey(0)
+    plt.clf()
+    cv2.destroyAllWindows()
+    edges = cv2.Canny(h,25,50)
+    cv2.imshow('edges',edges)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    
