e28d2563d3dca02c5bfd8df29b0297a1fb58e4fc
joaquincasanova <joaquin.casanova@gmail.com>
2015-04-22 11:43:54 -0400

    tmp007

--- py/ffcv.py
+++ py/ffcv.py
@@ -1,9 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import cv2
 
-for imnum in range(1,6):
+for imnum in range(0,4):
 
     imname = "../images/a{}.jpg".format(imnum) 
 
@@ -26,25 +24,32 @@ for imnum in range(1,6):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     hist = cv2.calcHist( [hsv], [0, 2], None, [180, 256], [0, 180, 0, 256] )
-    cv2.imshow('hist',hist)
+##    cv2.imshow('image',hist)
+##    cv2.waitKey(0)
+##    cv2.destroyAllWindows()
+##    heq = cv2.equalizeHist(h)
+##    cv2.imshow('image',heq)
+##    cv2.waitKey(0)
+##    cv2.destroyAllWindows()
+    histnorm = np.uint8(normalize(hist, 0, 255))
+    cv2.imshow('image',histnorm)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    H = np.linspace(0, 180, 180)
-    V = np.linspace(0, 256, 256)
-    VV,HH = np.meshgrid(V,H)
-    print hist.shape
-    ax.plot_surface(HH,VV,hist)
-    plt.show()
-    cv2.waitKey(0)
-    plt.clf()
-    cv2.destroyAllWindows()
-    edges = cv2.Canny(h,25,50)
-    cv2.imshow('edges',edges)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+##    fig = plt.figure()
+##    ax = fig.add_subplot(111, projection='3d')
+##
+##    H = np.linspace(0, 180, 180)
+##    V = np.linspace(0, 256, 256)
+##    VV,HH = np.meshgrid(V,H)
+##    print hist.shape
+##    ax.plot_surface(HH,VV,hist)
+##    plt.show()
+##    cv2.waitKey(0)
+##    plt.clf()
+##    cv2.destroyAllWindows()
+##    edges = cv2.Canny(heq,25,50)
+##    cv2.imshow('edges',edges)
+##    cv2.waitKey(0)
+##    cv2.destroyAllWindows()
 
     
