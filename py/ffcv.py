a3d3585f2d7454b91e0c48cc108a1ab2d3bc7baf
joaquincasanova <joaquin.casanova@gmail.com>
2015-04-27 18:29:35 -0400

    gr

--- py/ffcv.py
+++ py/ffcv.py
@@ -37,37 +37,37 @@ def readsplit(imname):
     img = cv2.imread(imname)
     cv2.imshow('image',img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h,s,v = cv2.split(hsv)
     b,g,r = cv2.split(img)
     
-    return h, s, v
+    return b, g, r
     
 for imnum in ['a', 'r']:
     imname = "../images/{}0.jpg".format(imnum) 
-    h,s,v=readsplit(imname)
+    b,g,r=readsplit(imname)
     n=3
-    features = channelops(h, n)
-    features = np.hstack((features,channelops(v, n)))
+    features = channelops(g, n)
+    features = np.hstack((features,channelops(r, n)))
     for n in [13]:
-        features = np.hstack((features,channelops(h, n)))
-        features = np.hstack((features,channelops(v, n)))
+        features = np.hstack((features,channelops(g, n)))
+        features = np.hstack((features,channelops(r, n)))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     ret, labels, centers = cv2.kmeans(features, 4, criteria, 1, cv2.KMEANS_RANDOM_CENTERS)
-    labels = labels.reshape(h.shape)
+    labels = labels.reshape(g.shape)
     B=(labels==0)*255
     G=(labels==1)*255
     R=(labels==2)*255
     segment=np.uint8(cv2.merge((B,G,R)))
     cv2.imshow('segment',np.uint8(segment))
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
     em = cv2.EM(4,cv2.EM_COV_MAT_DIAGONAL)
     ret, ll, labels, probs = em.train(features)
-    labels = labels.reshape(h.shape)
-    B=(labels==0)*255
+    labels = labels.reshape(g.shape)
+    B=(labels==0)*2550
     G=(labels==1)*255
     R=(labels==2)*255
     segment=np.uint8(cv2.merge((B,G,R)))
