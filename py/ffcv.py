77988c785a432ec3277b6ff64795f2f2936b40e8
joaquincasanova <joaquin.casanova@gmail.com>
2015-05-01 13:35:05 -0400

    4 group em

--- py/ffcv.py
+++ py/ffcv.py
@@ -36,7 +36,28 @@ def readsplit(imname):
     b,g,r = cv2.split(img)
     ndvi = (r-b)/(r+b)
     return h, s, v, img
-    
+
+def opening_adjust(mat):
+
+    kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+
+    cv2.namedWindow('open')
+    opening = cv2.morphologyEx(mat, cv2.MORPH_OPEN, kernel)
+    cv2.createTrackbar('n','open',0,191,nothing)
+    while(1):
+        cv2.imshow('open',opening)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+        n = cv2.getTrackbarPos('n','open')
+        kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(n,n))
+        opening = cv2.morphologyEx(mat, cv2.MORPH_OPEN, kernel)
+
+    cv2.destroyAllWindows()
+
+    return opening, n
+
 def bilat_adjust(mat):
 
     bilat = cv2.bilateralFilter(mat, 7, 7, 7)
@@ -132,60 +153,58 @@ labelsname = "../images/al0.jpg"
 trainname = "../images/at0.jpg" 
 testname = "../images/a0.jpg" 
 
-c1,c2,c3,img=readsplit(trainname)
+c1,c2,c3,img=readsplit(testname)
 bilat, n, sigC, sigD = bilat_adjust(c1)
-contours, minval, maxval = canny_contours(c1, n)
+test = channelops(c1, n, sigC, sigD)
+test = np.hstack((test,channelops(c3, n, sigC, sigD)))
+rows, cols = c1.shape
+cv2.namedWindow('image')
+cv2.imshow('image',img)
+cv2.waitKey(1)    
 
+c1,c2,c3,img=readsplit(trainname)
 train = channelops(c1, n, sigC, sigD)
 train = np.hstack((train,channelops(c3, n, sigC, sigD)))
 
 segment = cv2.imread(labelsname)
 labels = rgb_to_labels_2(segment)
 
-c1,c2,c3,img=readsplit(testname)
-
-test = channelops(c1, n, sigC, sigD)
-test = np.hstack((test,channelops(c3, n, sigC, sigD)))
-
-rows, cols = c1.shape
-cv2.namedWindow('image')
-cv2.imshow('image',img)
-cv2.waitKey(1)    
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-
       
-knn = cv2.KNearest()
+##knn = cv2.KNearest()
 em = cv2.EM(2,cv2.EM_COV_MAT_DIAGONAL)
-nb = cv2.NormalBayesClassifier()    
-svm_params = dict( kernel_type = cv2.SVM_RBF,
-                    svm_type = cv2.SVM_C_SVC)
-nb.train(train,labels)
-knn.train(train,labels)
+em2 = cv2.EM(4,cv2.EM_COV_MAT_DIAGONAL)
 
-varIdx=None
-samplesIdx=None
-svm = cv2.SVM()
-svm.train_auto(train,labels,varIdx,samplesIdx, params=svm_params)
-svm.save('svm_data.dat')
+##nb = cv2.NormalBayesClassifier()    
+##svm_params = dict( kernel_type = cv2.SVM_RBF,
+##                    svm_type = cv2.SVM_C_SVC)
+##nb.train(train,labels)
+##knn.train(train,labels)
 
-result = svm.predict_all(test)
-segment=labels_to_rgb_2(result,rows,cols)
-cv2.namedWindow("segment svm")
-cv2.imshow("segment svm",segment)
-cv2.waitKey(1)
-
-knnk = 25
-ret, result,neighbours,dist = knn.find_nearest(test,k=knnk)
-segment=labels_to_rgb_2(result,rows,cols)
-cv2.namedWindow("segment knn")
-cv2.imshow("segment knn",segment)
-cv2.waitKey(1)
-
-ret, result=nb.predict(test)
-segment=labels_to_rgb_2(result,rows,cols)
-cv2.namedWindow('segments nb')
-cv2.imshow('segments nb',segment)
-cv2.waitKey(1)
+##varIdx=None
+##samplesIdx=None
+##svm = cv2.SVM()
+##svm.train_auto(train,labels,varIdx,samplesIdx, params=svm_params)
+##svm.save('svm_data.dat')
+##
+##result = svm.predict_all(test)
+##segment=labels_to_rgb_2(result,rows,cols)
+##cv2.namedWindow("segment svm")
+##cv2.imshow("segment svm",segment)
+##cv2.waitKey(1)
+##
+##knnk = 25
+##ret, result,neighbours,dist = knn.find_nearest(test,k=knnk)
+##segment=labels_to_rgb_2(result,rows,cols)
+##cv2.namedWindow("segment knn")
+##cv2.imshow("segment knn",segment)
+##cv2.waitKey(1)
+##
+##ret, result=nb.predict(test)
+##segment=labels_to_rgb_2(result,rows,cols)
+##cv2.namedWindow('segments nb')
+##cv2.imshow('segments nb',segment)
+##cv2.waitKey(1)
 
 ret, ll, result, probs = em.train(test)
 segment=labels_to_rgb_2(result,rows,cols)
@@ -193,10 +212,17 @@ cv2.namedWindow('segments em')
 cv2.imshow('segments em',segment)
 cv2.waitKey(1)
 
-ret, result, centers = cv2.kmeans(test, 2, criteria, 1, cv2.KMEANS_RANDOM_CENTERS)
-segment=labels_to_rgb_2(result,rows,cols)
-cv2.namedWindow('segments kmeans')
-cv2.imshow('segments kmeans',segment)
-cv2.waitKey(0)    
-cv2.destroyAllWindows()
+ret, ll, result, probs = em2.train(test)
+segment=labels_to_rgb(result,rows,cols)
+cv2.namedWindow('segments em 4')
+cv2.imshow('segments em 4',segment)
+cv2.waitKey(1)
+
+##ret, result, centers = cv2.kmeans(test, 2, criteria, 1, cv2.KMEANS_RANDOM_CENTERS)
+##segment=labels_to_rgb_2(result,rows,cols)
+##cv2.namedWindow('segments kmeans')
+##cv2.imshow('segments kmeans',segment)
+##cv2.waitKey(0)    
+##cv2.destroyAllWindows()
 #
+opening, no = opening_adjust(segment)
