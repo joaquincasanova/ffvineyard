b979fc97538beb1fbb576eeb5833d32b5ad5bfa0
joaquincasanova <joaquin.casanova@gmail.com>
2015-06-15 15:17:46 -0400

    adaptive bilateral filter

--- py/drone.py
+++ py/drone.py
@@ -1,7 +1,47 @@
 import cv2
 import numpy as np
 import os
+
+def opening_adjust(mat):
+
+    kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+
+    cv2.namedWindow('open')
+    opening = cv2.morphologyEx(mat, cv2.MORPH_OPEN, kernel)
+    cv2.createTrackbar('n','open',0,50,nothing)
+    while(1):
+        cv2.imshow('open',opening)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+        n = cv2.getTrackbarPos('n','open')
+        kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2*n+1,2*n+1))
+        opening = cv2.morphologyEx(mat, cv2.MORPH_OPEN, kernel)
+
+    cv2.destroyAllWindows()
+
+    return opening, n
+
+def thresh_adjust(mat):
+    retval,thresh = cv2.threshold(mat,127,255,cv2.THRESH_BINARY_INV)
     
+    cv2.namedWindow('thresh',cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('T','thresh',0,255,nothing)
+    while(1):
+        cv2.imshow('thresh',thresh)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+            # get current positions of four trackbars
+        T = cv2.getTrackbarPos('T','thresh')
+        retval,thresh = cv2.threshold(mat,T,255,cv2.THRESH_BINARY_INV)    
+        
+    cv2.destroyAllWindows()
+
+    return thresh, T
+
 def fast_adjust(mat):
     fast = cv2.FastFeatureDetector(50)
     kp = fast.detect(mat,None)
@@ -71,6 +111,26 @@ def bilat_adjust(mat):
 
     return bilat, sigC, sigD
 
+def abf_adjust(mat):
+
+    bilat = cv2.adaptiveBilateralFilter(mat, (29,29), 7)
+
+    cv2.namedWindow('bilat',cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('sigD','bilat',0,30,nothing)
+    while(1):
+        cv2.imshow('bilat',bilat)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+        sigD = cv2.getTrackbarPos('sigD','bilat')
+        ksize=(4*sigD+1,4*sigD+1)
+        bilat = cv2.adaptiveBilateralFilter(mat, ksize, sigD)
+
+    cv2.destroyAllWindows()
+
+    return bilat, sigD
+
 def nothing(x):
     pass
 
@@ -93,22 +153,28 @@ def testwrite(img, pfx, imnum, i, j):
 
     if i==None:
         if imnum < 100:
-            oname = "../images/{}00790{}.JPG".format(pfx,imnum) 
+            oname = "../images/out/{}00790{}.JPG".format(pfx,imnum) 
         else:
-            oname = "../images/{}0079{}.JPG".format(pfx,imnum) 
+            oname = "../images/out/{}0079{}.JPG".format(pfx,imnum) 
         retval=cv2.imwrite(oname,img)
         print "Wrote ", oname, retval        
     else:   
         if imnum < 100:
-            oname = "../images/{}00790{}_{}_{}.JPG".format(pfx,imnum,i,j) 
+            oname = "../images/out/{}00790{}_{}_{}.JPG".format(pfx,imnum,i,j) 
         else:
-            oname = "../images/{}0079{}_{}_{}.JPG".format(pfx,imnum,i,j) 
+            oname = "../images/out/{}0079{}_{}_{}.JPG".format(pfx,imnum,i,j) 
         retval=cv2.imwrite(oname,img)
         print "Wrote ", oname, retval
 
 os.system("rm ../images/*A*.JPG")
 os.system("rm ../images/*H*.JPG")
+os.system("rm ../images/*a*.JPG")
+os.system("rm ../images/*L*.JPG")
+os.system("rm ../images/*S*.JPG")
+os.system("rm ../images/*V*.JPG")
 os.system("rm ../images/*GC*.JPG")
+os.system("rm ../images/*B*.JPG")
+os.system("rm ../images/*b*.JPG")
 
 imnum=34
 imnum_max=136
@@ -132,20 +198,26 @@ while imnum<=264:
     testwrite(h, "H", imnum, None, None)
         
     if imnum==34:
-        ab, sigC, sigD = bilat_adjust(a)
+        #ab, sigD = abf_adjust(a)
+        sigD = 12 
+        ksize=(4*sigD+1,4*sigD+1)
+        ab = cv2.adaptiveBilateralFilter(a, ksize, sigD)
         af, T = fast_adjust(ab) 
-        ae, n, minval, maxval = canny_adjust(ab)
+        #ae, n, minval, maxval = canny_adjust(ab)
+        at, t = thresh_adjust(ab)
         testwrite(ab, "AB", imnum, None, None)
         testwrite(af, "AF", imnum, None, None)
-        testwrite(ae, "AE", imnum, None, None)
+        testwrite(at, "AT", imnum, None, None)
     else:
-        ab = cv2.bilateralFilter(a, -1, sigC, sigD)
-        ae = cv2.Canny(ab,minval,maxval,n)
+        ksize=(4*sigD+1,4*sigD+1)
+        ab = cv2.adaptiveBilateralFilter(a, ksize, sigD)
+        #ae = cv2.Canny(ab,minval,maxval,n)
+        retval,at = cv2.threshold(ab,t,255,cv2.THRESH_BINARY_INV)
         fast = cv2.FastFeatureDetector(T)
         kp = fast.detect(ab,None)
         af = cv2.drawKeypoints(ab, kp, color=(255,0,0))
         testwrite(ab, "AB", imnum, None, None)
-        testwrite(ae, "AE", imnum, None, None)
+        testwrite(at, "AT", imnum, None, None)
         testwrite(af, "AF", imnum, None, None)
 
     img = None
