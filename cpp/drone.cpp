47dd1e0afe2bd7fe944f8c5f2196dd8f1b76e73c
joaquincasanova <joaquin.casanova@gmail.com>
2015-06-22 04:54:20 -0400

    campbell norman eq

--- cpp/drone.cpp
+++ cpp/drone.cpp
@@ -15,7 +15,7 @@ double pi = 3.1459265;
 double boltz = 0.00000005670373;// W K-4 m-2
 double vonk = 0.4;
 double Tk = 273.16;
-double gamma = 0.000665;
+double gammac = 0.000665;
 double cp = 1005; //J/kg/K
 double rhoa = 1.205; //kg/m3
  
@@ -31,16 +31,16 @@ public:
   double lat;//deg
   double lonm;//deg-longitude of measurement site
   double lonz;//deg
-  double RsIn;//MJ/m2/h
+  double SIn;//MJ/m2/h
   int J(void){return D-32+(int)(275*M/9)+2*(int)(3/(M+1))+(int)(M/100-(Y%4)/4+0.975);}
-  double Sc(void){
+  double sc(void){
     double b=2*pi*(J()-81)/364;
     return 0.1645*sin(2*b)-0.1255*cos(b)-0.025*sin(b);
   }//hour
   double dr(void){return 1+0.033*cos(2*pi/365*J());}
   double dec(void){return 0.409*sin(2*pi/365*J()-1.39);}
   double phi(void){return pi/180*lat;}
-  double omega(void){return pi/12*(H+0.06667*(lonz-lonm)+Sc()-12);}
+  double omega(void){return pi/12*(H+0.06667*(lonz-lonm)+sc()-12);}
   double omega_s(void){return acos(-tan(phi())*tan(dec()));}
   double omega_1(void){
     double tmp=omega()-pi*H/24; 
@@ -65,29 +65,29 @@ public:
   double beta(void){
     return asin(sin(phi())*sin(dec())+cos(phi())*cos(dec())*cos(omega()));
   }
-  double Rs(void){
+  double S(void){
     double tmp1 = (omega_2()-min(omega_1(),omega_2()))*sin(phi())*sin(dec());
     double tmp2 = (sin(omega_2()-min(omega_1(),omega_2())))*cos(phi())*cos(dec());
     return 12/pi*0.0820*dr()*(tmp1+tmp2);
   }
   double fcd(void){
-    return (1.35*Rs()/Rso()-0.35);
+    return (1.35*S()/o()-0.35);
   }
   double fcd_beta_lt_03(void){
     int H0 = H;
     H=1;
     while(beta()<0.3){H++;}
-    double tmp = (1.35*Rs()/Rso()-0.35);
+    double tmp = (1.35*S()/So()-0.35);
     H = H0;
     return tmp;
   }
-  double Rso(void){
-    return (0.75+(2e-5)*ZZ)*Rs();}
-  double Rns(double alb){
-    if(RsIn<0){return Rso()*(1.0-alb);}
-    else{return RsIn*(1.0-alb);}
+  double So(void){
+    return (0.75+(2e-5)*ZZ)*S();}
+  double Sn(double alb){
+    if(SIn<0){return So()*(1.0-alb);}
+    else{return SIn*(1.0-alb);}
   }
-  double Rnl(void){
+  double Ln(void){
     if (beta()>=0.3){
       return boltzh*pow(Ta+Tk,4)*(0.34-0.14*sqrt(e_a()))*fcd();
     }else{
@@ -95,12 +95,11 @@ public:
     }
   }
   double eps_atm(void){return 0.70+0.000595*e_a()*exp(1500/(Ta+Tk));}
-  double Rn(void){return Rns()-Rnl();}//MJ/m2/h
+  double Rn(void){return Sn()-Ln();}//MJ/m2/h
   double Lsky(void){return eps_atm()*boltz*pow(Ta+Tk,4.0);}
-  
 };
 
-Rad::Rad(int YY, int MM, int DD, int HH, double llat, double llonm, double llonz, double RRsIn){
+Rad::Rad(int YY, int MM, int DD, int HH, double llat, double llonm, double llonz, double SSIn){
   int D=DD;
   int M=MM;
   int Y=YY;
@@ -108,7 +107,7 @@ Rad::Rad(int YY, int MM, int DD, int HH, double llat, double llonm, double llonz
   double lat=llat;//deg
   double lonm=llonm;//deg-longitude of measurement site
   double lonz=llonz;//deg
-  double RsIn=RRsIn;//MJ/m2/h
+  double SIn=SSIn;//MJ/m2/h
 }
 
 Rad::~Rad(void){
@@ -135,17 +134,10 @@ public:
   double uz;//m/s
   double z;//m
   double ZZ;
-  double Tdry(double d, double z0, double Rn){return Ta+ra(d, z0)*Rn/(rhoa*cp);}
-  double Twet(double rc, double d, double z0, double Rn){
-    double tmp1 = rc*ra(d,z0)*gamma*Rn/(rhoa*cp*(gamma*rc+delta()*ra(d,z0)));
-    double tmp2 = ra(d,z0)*(e_s()-e_a())/(gamma*rc+delta()*ra(d,z0));
-    return Ta + tmp1 - tmp2;
-  }
-
-  //private:
   double u2(void){if(z==2){return uz;}else{return uz*4.87/log(67.8*z-5.42);}} //m/s, m
   double delta(void){return 4098*(0.6108*exp(17.27*Ta/(Ta+237.7)))/pow(Ta+273.3,2);} //kpa/C
   double P(void){return 101.3*pow(((293-0.0065*ZZ)/293),5.26);}//kpa, 
+  double gamma(void){return P()*gammac;}//kpa/c
   double DT(void){return delta()/(delta()+gamma*(1+0.34*u2()));}
   double PT(void){return gamma/(delta()+gamma*(1+0.34*u2()));}
   double TT(void){return (900/(Ta+273))*u2();}
@@ -155,7 +147,7 @@ public:
   double zm(double hc){return 0.13*hc;}
   double zh(double hc){return zm(hc)/7;}
   double d(double hc){return 0.65*hc;}
-    double ra(double hc){return 4.72*pow((log((z-d(hc))/zm(hc))/vonk),2)/(1+0.54*uz);}
+  double ra(double hc){return log((z-d(hc))/zm(hc))*log((z-d(hc))/zh(hc))/(vonk*vonk)/rhoa/cp/uz;}//C&N 1998, nutral stability
 };
 
 Air::Air(double TTa, double RRH, double uuz, double zz, double ZZZ){
@@ -204,6 +196,7 @@ public:
     double a = 0.28*pow(LAI,2.0/3.0)*pow(hc/leaf,1.0/3.0);
     double udzm = uc()*exp(-a*(1-(d+zm())/hc));
     return C/LAI()*sqrt(leaf/udzm);}
+  double Rnc(double LAI){return ;}//????
   double T(double Tirt, double Ts, double Lsky){
     double eps_c = 0.98;
     double eps_s = 0.98;
@@ -229,12 +222,19 @@ Canopy::Canopy(double row1, double  wc1, double fc1, double ff1, double hc1){
 
 Canopy::~Canopy(void){
 }
-  
+
+double gamma_star(double gamma, double ra, double rc){return gamma*(rc/ra);}//c&n 1998//kpa/c
+double Tdry(double d, double z0, double Rnc){return Ta+ra*Rnc/(rhoa*cp);}
+double Twet(double rc, double d, double z0, double Rnc, double gamma_star){
+  double tmp1 = ra*gamma_star*Rnc/(rhoa*cp*(gamma_star+delta()));
+  double tmp2 = (e_s()-e_a())/(gamma_star+delta());
+  return Ta + tmp1 - tmp2;
+}
 
 int main(void){
 
   Air air(Ta, RH, uz, z, ZZ)
-  Rad rad(Y, M, D, H, lat, lonm, lonz, RsIn)
+  Rad rad(Y, M, D, H, lat, lonm, lonz, SIn)
   Canopy can(row, wc, fc, ff, hc);
    
   ifstream ifile;
