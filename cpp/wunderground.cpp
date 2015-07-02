ebc08f4b9b84307c3a725e81241f552b3d53dafe
joaquincasanova <joaquin.casanova@gmail.com>
2015-07-02 05:23:45 -0400

    tinyxml

--- cpp/wunderground.cpp
+++ cpp/wunderground.cpp
@@ -4,14 +4,34 @@
 #include <fstream>
 #include <string.h>
 #include <math.h>
-#include <rapidxml.hpp>
+#include <vector>
+#include <tinyxml2.h>
 
 using namespace std;
-using namespace rapidxml;
+using namespace tinyxml2;
 
 int main(void){
 
   //pull from Wunderground
-  int ret = system("wget http://api.wunderground.com/api/eea590fdddcc01bb/conditions/q/pws:KOKCYRIL3.xml");
+  int ret = system("rm pws:KOKCYRIL3.xml*");
+  ret = system("wget http://api.wunderground.com/api/eea590fdddcc01bb/conditions/q/pws:KOKCYRIL3.xml");
+
+  XMLDocument doc;
+  doc.LoadFile( "pws:KOKCYRIL3.xml" );
+
+  
+  cout << "Parsing..." << endl;
+
+  
+  /*local_time_rfc822
+latitude
+longitude
+elevation
+temp_c
+relative_humidity
+wind_kph
+pressure_mb
+solarradiation*/
+
   return 0;
 }
